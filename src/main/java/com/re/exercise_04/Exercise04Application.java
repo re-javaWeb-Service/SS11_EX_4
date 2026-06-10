3702361203be19783a13cd8f58722c5cac2a56ab
lochuynh22 <huynhlatienloc123@gmail.com>
2026-06-10 14:08:23 +0700

    init

---
+++ src/main/java/com/re/exercise_04/Exercise04Application.java
@@ -0,0 +1,13 @@
+package com.re.exercise_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise04Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/re/exercise_04/Exercise04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.re.exercise_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
